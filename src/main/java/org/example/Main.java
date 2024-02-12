13972173801ca10e6fcc43639d1ef54d337f0754
Khalid M. Sheet <khalid.m.sheet@gmail.com>
2024-02-12 18:15:01 +0300

    Feature: :sparkles: isPalidrome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
 
+import static org.example.Palindrome.isPalindrome;
+
 public class Main {
 
     public static void main(String[] args) {
+        int x1 = 121;
+        int x2 = 10;
 
+        System.out.println("Input: x = " + x1 + ", Output: " + isPalindrome(x1));
+        System.out.println("Input: x = " + x2 + ", Output: " + isPalindrome(x2));
     }
 }
