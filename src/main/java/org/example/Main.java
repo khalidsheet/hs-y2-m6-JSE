1657d27bb68803aea7caeef5ce164ad3b4182c09
Khalid M. Sheet <khalid.m.sheet@gmail.com>
2024-02-12 18:20:22 +0300

    Feature: :sparkles: ValidParentheses

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,17 @@
 package org.example;
 
 
+import static org.example.ValidParentheses.isValid;
+
 public class Main {
 
     public static void main(String[] args) {
+        String s1 = "()[]{}";
+        String s2 = "(]";
+        String s3 = "()[";
 
+        System.out.println("Input: s = \"" + s1 + "\", Output: " + isValid(s1));
+        System.out.println("Input: s = \"" + s2 + "\", Output: " + isValid(s2));
+        System.out.println("Input: s = \"" + s3 + "\", Output: " + isValid(s3));
     }
 }
